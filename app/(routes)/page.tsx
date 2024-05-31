af8d231f155441ffbe46e0fb02aed30dba0b612a
mzayem <mzayemazam@gmail.com>
2024-05-31 21:59:48 +0500

    creating landing pages
    setup the billboard and creating feature product section

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,26 @@
-export default function HomePage() {
-  return <div>hello store</div>;
+import getBillboards from "@/actions/get-billboard";
+import getProducts from "@/actions/get-product";
+
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+export default async function HomePage() {
+  const products = await getProducts({ isFeatured: true });
+
+  const billboards = await getBillboards(
+    "b4eb6bcb-598c-4555-b58b-d119996112a7"
+  );
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboards} />
+      </div>
+      <div className="flex flex-col gap-y-9 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 }
---
+++ components/ui/no-result.tsx
@@ -0,0 +1,7 @@
+export default function NoResults() {
+  return (
+    <div className="flex items-center justify-center h-full w-full">
+      <div>No result found!</div>
+    </div>
+  );
+}
